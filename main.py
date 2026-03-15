d1a9c0532909f6d095ad09e1b3df3b9e2129fd2c
worfeva <worfeva@gmail.com>
2026-03-15 20:05:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -366,7 +366,7 @@ async def read_reviews(update: Update, context: ContextTypes.DEFAULT_TYPE, messa
             await update.message.reply_text("Пока нет одобренных отзывов.")
         return READING
     keyboard = []
-    for review_id, title, rating, nickname in reviews
+    for review_id, title, rating, nickname in reviews:
 	keyboard.append([
 	    InlineKeyboardButton(f"{title} ({'⭐' * rating}) — {nickname}", callback_data=f"user_read_{review_id}"),
 	    InlineKeyboardButton("❌ Отмена", callback_data="cancel")
